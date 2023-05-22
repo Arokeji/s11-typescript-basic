311f2dfb9e28ef58d93945b8ec3d0df861a11dd5
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-05-22 13:10:30 +0200

    Arranque

---
+++ src/cuentas.ts
@@ -0,0 +1,14 @@
+let titular: string;
+let cantidad: number;
+
+const ingresar = (cantidadIngresar: number) => {
+
+}
+
+const retirar = (cantidadRetirar: number) => {
+    
+}
+
+const mostrar = () => {
+
+}
